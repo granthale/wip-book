e826506c79af0761da907b0dd98afebe9a0d64b0
Grant Hale <64755492+granthale@users.noreply.github.com>
2024-01-27 19:47:08 -0600

    Updated landing page aesthetics

--- content/tools-for-thought.md
+++ content/tools-for-thought.md
@@ -2,6 +2,12 @@
 title: Tools for Thought
 ---
 
-Welcome to a page on using technology to think and learn more effectively
+Welcome to a page on using technology to think and learn more effectively.
+
+This section aims to answer three questions
+
+1. [[How can LLMs be used to extend pre-existing flashcards (Anki or otherwise) to push our understanding and facilitate learning?]]
+2. [[How to incentivize big-tech investments in Tools for Thought? Why are Tools for Thought important?]]
+3. [[How do we get people to examine what they know more often?]]
 
 See [[example]]
--- quartz/components/Landing.tsx
+++ quartz/components/Landing.tsx
@@ -1,41 +1,46 @@
 import { QuartzComponentConstructor } from "./types"
 import landingStyle from "./styles/landing.scss"
-import Search from "./Search"
 
-export const TOTAL_CARDS = 12
+export const TOTAL_CARDS = 4
 export const CARDS = {
-  basics: (
+  tools_for_thought: (
     <a href={"/tools-for-thought"}>
       <div class="card card-1">
         <p class="card-title">Tools for Thought</p>
       </div>
     </a>
   ),
-  "getting-started": (
-    <a href={"/internet-n-tech-history"}>
+  tech_history: (
+    <a href={"/tech-history"}>
       <div class="card card-2">
-        <p class="card-title">Internet & Technology History</p>
+        <p class="card-title">Internet & Tech History</p>
       </div>
     </a>
   ),
-  "growing-people": (
+  global_economy: (
     <a href={"/global-economy"}>
       <div class="card card-3">
         <p class="card-title">Global Economy</p>
       </div>
     </a>
   ),
+  energy: (
+    <a href={"/energy"}>
+      <div class="card card-4">
+        <p class="card-title">Energy</p>
+      </div>
+    </a>
+  ),
 }
 
 export default (() => {
-
   function LandingComponent() {
     return (
       <div>
         <div class="content-container">
-        <div class="landing-header">Hello, welcome to our garden.</div>
-        <br />
-        {/* TODO, add search and navbar */}
+          <div class="landing-header">Hello, welcome to our garden.</div>
+          <p class="page-subhead">Check the <a href="/about">about section</a> to learn more.</p>
+          {/* TODO, add search and navbar */}
           <div class="issue-container">
             {Object.values(CARDS)}
             {Array(TOTAL_CARDS - Object.keys(CARDS).length)
