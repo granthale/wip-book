37c5877d1326cc95f3c4897aa4d28eae0bda3c2c
Grant Hale <64755492+granthale@users.noreply.github.com>
2024-02-10 17:43:30 -0600

    Added in soil and welcome banner

--- quartz/components/Landing.tsx
+++ quartz/components/Landing.tsx
@@ -3,34 +3,31 @@ import landingStyle from "./styles/landing.scss"
 import RecentNotes from "./RecentNotes"
 import { SimpleSlug } from "../util/path"
 
-export const TOTAL_CARDS = 4
+export const TOTAL_CARDS = 5
 export const CARDS = {
   tools_for_thought: (
-    <a href={"Acres/tools-for-thought"}>
-      <div class="card card-1">
-        <p class="card-title">Tools for Thought</p>
-      </div>
+    <a href={"Acres/tools-for-thought"} class="card card-1">
+      <p class="card-title">Tools for Thought</p>
     </a>
   ),
   tech_history: (
-    <a href={"Acres/tech-history"}>
-      <div class="card card-2">
-        <p class="card-title">Internet & Computer History</p>
-      </div>
+    <a href={"Acres/tech-history"} class="card card-2">
+      <p class="card-title">Internet & Computer History</p>
     </a>
   ),
-  global_economy: (
-    <a href={"Acres/economics"}>
-      <div class="card card-3">
-        <p class="card-title">Economics</p>
-      </div>
+  economics: (
+    <a href={"Acres/economics"} class="card card-3">
+      <p class="card-title">Economics</p>
     </a>
   ),
   energy: (
-    <a href={"Acres/energy"}>
-      <div class="card card-4">
-        <p class="card-title">Energy</p>
-      </div>
+    <a href={"Acres/energy"} class="card card-4">
+      <p class="card-title">Energy</p>
+    </a>
+  ),
+  soil: (
+    <a href={"/Soil"} class="card card-soil">
+      <p className="card-title">Soil</p>
     </a>
   ),
 }
@@ -54,28 +51,25 @@ export default (() => {
     return (
       <div class="landing">
         <div class="content-container">
-          {/* <div class="landing-header">Hello, welcome to our garden.</div>
+          <div class="landing-header">Hello, welcome to our garden.</div>
           <p class="page-subhead">
             Check the <a href="/about">about section</a> to learn more.
-          </p> */}
+          </p>
           <div class="flexer">
             <div class="recent-notes">
+              <br />
               <RecentWritingComponent {...componentData} />
               <RecentNotesComponent {...componentData} />
             </div>
-            <div class="about">
-              {/* <hr class="solid"></hr> */}
-              <br />
-              <div class="issue-container">
-                {Object.values(CARDS)}
-                {Array(TOTAL_CARDS - Object.keys(CARDS).length)
-                  .fill(0)
-                  .map(() => (
-                    <div class="card card-coming">
-                      <p class="card-title">Coming Soon</p>
-                    </div>
-                  ))}
-              </div>
+            <div class="issue-container">
+              {Object.values(CARDS)}
+              {/* {Array(TOTAL_CARDS - Object.keys(CARDS).length)
+                .fill(0)
+                .map(() => (
+                  <div class="card card-coming">
+                    <p class="card-title">Coming Soon</p>
+                  </div>
+                ))} */}
             </div>
           </div>
         </div>
