f0536df6b00eeb6f59bbf90292e181f424af1f10
Grant Hale <64755492+granthale@users.noreply.github.com>
2024-02-15 15:58:46 -0600

    Hopefully removed slight right shift on landing page load

--- quartz/components/Head.tsx
+++ quartz/components/Head.tsx
@@ -19,12 +19,12 @@ export default (() => {
       <head>
         <title>{title}</title>
         <meta charSet="utf-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta name="viewport" content="width=device-width" />
         <meta property="og:title" content={title} />
         <meta property="og:description" content={description} />
         {cfg.baseUrl && <meta property="og:image" content={ogImagePath} />}
-        <meta property="og:width" content="1200" />
-        <meta property="og:height" content="675" />
+        {/* <meta property="og:width" content="1200" /> */}
+        {/* <meta property="og:height" content="675" /> */}
         <link rel="icon" href={iconPath} />
         <meta name="description" content={description} />
         <meta name="generator" content="Quartz" />
